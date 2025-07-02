3da81aec25db1ea2fe699ec650751d50501f1891
dkp2233 <dkp2233jira@gmail.com>
2025-07-02 11:01:04 +0530

    Update Firstprogram.java

--- dummyproject/src/dummyproject/Firstprogram.java
+++ dummyproject/src/dummyproject/Firstprogram.java
@@ -5,6 +5,7 @@ public class Firstprogram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("my first program for dummy");
+		System.out.println("i Done some notification by Using gitHub");
 	}
 
 }
